34890a519c6bee1ae91b8c3fc537f80a44705d2c
Eduardo Ritter <eduardoritter@hotmail.com>
2020-12-01 00:10:54 -0300

    backend done and frontend need style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Removing all
+puts '~~~~~~~~~~~~~~'
+puts 'Starting SEED'
+puts '~~~~~~~~~~~~~~'
+sleep(0.5)
+puts 'Step 1 - Destroying old DataBase'
+print '.'
+sleep(0.2)
+print '.'
+sleep(0.2)
+print '.'
+sleep(0.2)
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+puts 'Done!'
+puts ''
+puts 'Step 2 - Creating New INGREDIENTs'
+print '.'
+sleep(0.2)
+print '.'
+sleep(0.2)
+print '.'
+sleep(0.2)
+50.times do
+  new = Ingredient.create(
+    name: Faker::Food.ingredient
+  )
+  puts "<< #{new.name} - created"
+end
+puts 'Done!'
+puts ''
