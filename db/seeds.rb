c7e5ca60036ab208a1414f898e52f26094b21596
Eduardo Ritter <eduardoritter@hotmail.com>
2020-12-01 13:44:45 -0300

    root created

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,11 +22,42 @@ print '.'
 sleep(0.2)
 print '.'
 sleep(0.2)
-50.times do
+30.times do
   new = Ingredient.create(
     name: Faker::Food.ingredient
   )
   puts "<< #{new.name} - created"
+end
+puts 'Done!'
+puts ''
+
+puts 'Step 4 - Creating New CockTails!'
+print '.'
+sleep(0.2)
+print '.'
+sleep(0.2)
+print '.'
+sleep(0.2)
+30.times do
+  cock = Cocktail.create(
+    name: "#{Faker::Games::Pokemon.name} #{Faker::Games::Pokemon.move}"
+  )
+end
+puts 'Done!'
+puts ''
+
+puts 'Step 3 - Creating New DOSEs DESCRIPTIONs'
+print '.'
+sleep(0.2)
+print '.'
+sleep(0.2)
+print '.'
+sleep(0.2)
+30.times do
+  move = Dose.create(
+    description: Faker::Dessert.flavor
+  )
+  puts "# #{move.description} - created"
 end
 puts 'Done!'
 puts ''
